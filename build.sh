ebf128652a383add650b0acfa0f5c438afb3a7c8
fish_ball <57082212@qq.com>
2016-03-05 21:39:52 +0800

    Adding more supported libs

--- build.sh
+++ build.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
-apt-get update && apt-get install -y libpng12-dev libjpeg-dev 
+apt-get update
 
+# gd support
+apt-get install -y \
+    libpng12-dev \
+    libjpeg-dev
 docker-php-ext-configure gd --with-png-dir=/usr --with-jpeg-dir=/usr
+docker-php-ext-install gd
 
-docker-php-ext-install gd mysqli mysql opcache mbstring
-
+# opcache support
+docker-php-ext-install opcache
 cat <<EOF > /usr/local/etc/php/conf.d/opcache-recommended.ini
 opcache.memory_consumption=128
 opcache.interned_strings_buffer=8
@@ -14,3 +19,18 @@ opcache.revalidate_freq=60
 opcache.fast_shutdown=1
 opcache.enable_cli=1
 EOF
+
+# curl support
+apt-get install -y \
+    libcurl4-gnutls-dev
+docker-php-ext-install curl
+
+# mcrypt support
+apt-get install -y \
+    libmcrypt-dev
+docker-php-ext-install mcrypt
+
+# other php supports
+docker-php-ext-install mysqli
+
+
