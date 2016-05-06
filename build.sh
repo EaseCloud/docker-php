4cb45b5c58761bcdb18169380cb80c104d1dd0ba
Alfred Huang <57082212@qq.com>
2016-05-06 15:30:56 +0800

    添加了 zip 模块

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ apt-get update
 # gd support
 apt-get install -y \
     libpng12-dev \
-    libjpeg-dev
+    libjpeg-dev \
 docker-php-ext-configure gd --with-png-dir=/usr --with-jpeg-dir=/usr
 docker-php-ext-install gd
 
@@ -30,7 +30,12 @@ apt-get install -y \
     libmcrypt-dev
 docker-php-ext-install mcrypt
 
+# zip support
+apt-get install -y \
+    zlib1g-dev
+docker-php-ext-install zip
+
+
 # other php supports
 docker-php-ext-install mysqli mbstring
-
 
